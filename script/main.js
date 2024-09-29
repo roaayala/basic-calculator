0800a3d031872e1ba8d9190b9279fe9cc4d82175
roaayala <roaayala.ahm@gmail.com>
2024-09-29 12:55:38 +0800

    make function for math operation: adding, substraction, multiplication and division

--- script/main.js
+++ script/main.js
@@ -1,1 +1,9 @@
-console.log(this);
+const add = (x, y) => x + y;
+const subtract = (x, y) => x - y;
+const multiply = (x, y) => x * y;
+const divide = (x, y) => x / y;
+
+console.log(add(1, 2));
+console.log(subtract(1, 2));
+console.log(multiply(4, 2));
+console.log(divide(10, 5));
